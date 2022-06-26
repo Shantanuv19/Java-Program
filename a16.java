9d4641685eb6343d9f747258e8279d948902dc52
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:48:41 +0530

    Create a16.java

---
+++ a16.java
@@ -0,0 +1,15 @@
+//if
+import java.util.*;
+class test{
+	public static void main(String[] args)
+	{
+		int a,b;
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter 1st value:- ");
+		a=obj.nextInt();
+		System.out.println("Enter 2nd value:- ");
+		b=obj.nextInt();
+		if(a>b)
+		System.out.println("A is greater:- "+a);
+	}
+}
