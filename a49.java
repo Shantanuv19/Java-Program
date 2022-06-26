c03b8f2d0d6b75a8b0baaa626c5982966ace8f8c
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 12:02:23 +0530

    Create a49.java

---
+++ a49.java
@@ -0,0 +1,9 @@
+//current date
+import java.time.LocalTime;
+class test{
+	public static void main(String[] args)
+	{
+		LocalTime time=LocalTime.now();
+		System.out.println("Current time is:- "+time);
+	}
+}
