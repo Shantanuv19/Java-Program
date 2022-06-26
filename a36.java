ca5e87b0c471015eb3cfa39670c96dde2a2113d8
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:56:56 +0530

    Create a36.java

---
+++ a36.java
@@ -0,0 +1,22 @@
+// with return with parameter
+import java.util.*;
+class test{
+	int add(int a, int b)
+	{
+		
+		return (a+b);
+	}
+	public static void main(String[] args)
+	{
+		int x;
+		test obj=new test();
+		int a, b;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter 1st element:- ");
+		a=s.nextInt();
+		System.out.println("Enter 2nd element:- ");
+		b=s.nextInt();
+		x=obj.add(a,b);
+		System.out.println("Addition of two number is:- "+x);
+	}
+}
