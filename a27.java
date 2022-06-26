7c49628ec434015325a8942f2a9c26dfc9c1783b
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:53:19 +0530

    Create a27.java

---
+++ a27.java
@@ -0,0 +1,15 @@
+//continue
+import java.util.*;
+class test{
+	public static void main(String[] args)
+	{	
+		int arr[] = {1,2,3,4,5};
+		for(int a:arr)
+		{
+			if(a==2)
+			{
+			continue;}
+			System.out.println(a);
+		}
+	}
+}
