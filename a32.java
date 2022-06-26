a3d48bdd89733e63e2281f332d330a6844f5964b
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:55:10 +0530

    Create a32.java

---
+++ a32.java
@@ -0,0 +1,15 @@
+// double dimention arr
+class test{
+	public static void main(String [] args)
+	{
+		int arr[][] = {{1,2},{3,4}};
+		for(int a=0; a<2; a++)
+		{
+			for(int b=0; b<2; b++)
+			{
+			System.out.print(arr[a][b]+" ");
+			}
+		System.out.println();
+		}
+	}
+}
