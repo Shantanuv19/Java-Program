aae86b7a65f1fbddde0f8af2b78ef0b848499068
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:53:43 +0530

    Create a28.java

---
+++ a28.java
@@ -0,0 +1,14 @@
+//return statment
+import java.util.*;
+class test{
+	int add()
+	{
+		int a=2, b=2;
+		return (a+b);
+	}
+	public static void main(String args[])
+	{
+		test obj=new test();
+		System.out.println(obj.add());
+	}
+}
