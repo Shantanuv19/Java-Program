6d007666bde8d973fa6d75c43772ed35ff938819
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 12:01:58 +0530

    Create a48.java

---
+++ a48.java
@@ -0,0 +1,9 @@
+//current date
+import java.time.LocalDate;
+class test{
+	public static void main(String[] args)
+	{
+		LocalDate date=LocalDate.now();
+		System.out.println("Date is:- "+date);
+	}
+}
