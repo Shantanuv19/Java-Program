c773174e9a2b58d0726c03010599b5894211b911
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:54:03 +0530

    Create a29.java

---
+++ a29.java
@@ -0,0 +1,10 @@
+//for print all element
+import java.util.*;
+class test{
+	public static void main(String args[])
+	{
+		int arr[] = {1,2,3,4,5};
+		for(int a=1; a<=5; a++)
+		System.out.print(a+" ");
+	}
+}
