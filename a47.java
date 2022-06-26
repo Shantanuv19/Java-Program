94b4fcfceaab623821e5de195ca73ad2defe6c7f
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 12:01:39 +0530

    Create a47.java

---
+++ a47.java
@@ -0,0 +1,11 @@
+//take data from user
+import java.util.*;
+class test{
+	public static void main(String[] args)
+	{
+		String s;	
+		Scanner obj=new Scanner(System.in);
+		s=obj.nextLine();
+		System.out.println(s);
+	}
+}
