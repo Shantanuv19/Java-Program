420822a9e79cd2bd975c208cd5f686876d4fdd14
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:48:14 +0530

    Create a15.java

---
+++ a15.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class test{
+	public static void main(String[] args)
+	{
+		// Single line comment
+		System.out.println("// single");
+		/*multiple line comment*/
+		System.out.println("/* multiple */");
+	}
+}
