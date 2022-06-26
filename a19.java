12b11fc91ae1ec8f3ff3954b7eec7aabd5d1c025
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:49:32 +0530

    Create a19.java

---
+++ a19.java
@@ -0,0 +1,17 @@
+//if else if ladder
+import java.util.*;
+class test{
+	public static void main(String[] args)
+	{
+		int a,b;
+		Scanner obj=new Scanner(System.in);
+		System.out.println("Enter 1st value:- ");
+		a=obj.nextInt();
+		if(a>=0 && a<=10)
+		System.out.println("A is Between 0 - 10:- "+a);
+		else if(a>=11 && a<=20)
+		System.out.println("A is Between 10 - 20:- "+a);
+		else if(a>20 && a<=30)
+		System.out.println("A is betweeen 20 - 30:- "+a);
+	}
+}
