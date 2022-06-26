79c6768535e6c0adccb2b9745d8529b5d2a69516
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-06-26 11:47:46 +0530

    Create a14.java

---
+++ a14.java
@@ -0,0 +1,15 @@
+// conditional operator
+import java.util.*;
+class test{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter 1st value:- ");
+		a=s.nextInt();
+		System.out.println("Enter 2nd value:- ");
+		b=s.nextInt();
+		System.out.println("Greater value:- "+(a>b?a:b));
+
+	}
+}
